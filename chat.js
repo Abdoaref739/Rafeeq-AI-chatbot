71ab8ee99cfb0a2e7c8c94d4e82ae8c592803e61
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-07-22 11:40:59 +0300

    update API key handling and endpoint URL in AIResponse function

--- chat.js
+++ chat.js
@@ -30,8 +30,8 @@ function sendData(){
 
 
 async function AIResponse(){
-  const GROQ_API_KEY = "https://github.com/Abdoaref739/Rafeeq-AI-chatbot/security/secret-scanning/unblock-secret/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";
-  const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
+  const GROQ_API_KEY = "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";
+  const response = await fetch("https://github.com/Abdoaref739/Rafeeq-AI-chatbot/security/secret-scanning/unblock-secret/api.groq.com/openai/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
