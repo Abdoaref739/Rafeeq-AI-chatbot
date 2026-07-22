45a8e229e265a1cef698c77c9c53c4b9b3bee8d7
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-07-22 11:50:06 +0300

    replaced the api key

--- chat.js
+++ chat.js
@@ -30,7 +30,7 @@ function sendData(){
 
 
 async function AIResponse(){
-  const GROQ_API_KEY = "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";
+  const GROQ_API_KEY = "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";
   const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
     method: "POST",
     headers: {
