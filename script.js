dd618d26081f0d8a94b02d2b7a49493dbca7d635
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-07-23 14:48:56 +0300

    added the feature of sending messages via form

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let tryBtn = document.getElementById("try-button");
 let planBtn = document.getElementById("plan-btn");
+let submitBtn = document.getElementById("form-submit-button");
 tryBtn.addEventListener("click", function(){
     location.href = "./chat.html";
 });
+
 planBtn.addEventListener("click", function(){
     alert("Subscriptions coming soon!")
 })
+
+emailjs.init("CL192Y1b7AUbQ4nNB");
+submitBtn.onclick = function(event){
+    event.preventDefault();
+    let values = {
+    name: document.getElementById("form-name-field").value,
+    email: document.getElementById("form-email-field").value,
+    message: document.getElementById("form-message-field").value
+}
+    console.log(values)
+    emailjs.send("service_4u8zqjp", "template_aokp6xc", values);
+}
