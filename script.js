5909525fc8e44fed3e36e0025955f0956448d68d
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-07-23 11:52:35 +0300

    more details added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let tryBtn = document.getElementById("try-button");
+let planBtn = document.getElementById("plan-btn");
 tryBtn.addEventListener("click", function(){
     location.href = "./chat.html";
 });
+planBtn.addEventListener("click", function(){
+    alert("Subscriptions coming soon!")
+})
