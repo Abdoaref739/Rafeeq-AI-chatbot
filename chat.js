950716f7e7da0638d31f6de94640bc26cf4ebd2f
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-07-28 21:38:32 +0300

    api key push

--- chat.js
+++ chat.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 let sendIcon = document.getElementById("send-icon");
 let plusIcon = document.getElementById("plus-icon");
 let messagesDiv = document.getElementById("messages-div");
@@ -37,7 +36,7 @@ function sendData(){
 }
 }
 async function AIResponse(){
-  const GROQ_API_KEY = process.env.API_KEY;
+  const GROQ_API_KEY = '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';
   const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
     method: "POST",
     headers: {
---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+console.log(process.env.API_KEY);
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
