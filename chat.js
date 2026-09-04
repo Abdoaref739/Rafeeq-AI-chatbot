91e6af9ecd0086b12275cd6ca7b9b7dc71b4d7e6
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-09-04 15:50:39 +0300

    added the api key and some dependencies

--- chat.js
+++ chat.js
@@ -36,7 +36,7 @@ function sendData(){
 }
 }
 async function AIResponse(){
-  const GROQ_API_KEY = API_KEY;;
+  const GROQ_API_KEY = "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";
   const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
     method: "POST",
     headers: {
