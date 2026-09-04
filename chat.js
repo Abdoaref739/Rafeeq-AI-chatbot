80c56da6632733a06cc95dcd7fbad4e52decec97
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-09-04 15:39:16 +0300

    bug fixing

--- chat.js
+++ chat.js
@@ -36,7 +36,7 @@ function sendData(){
 }
 }
 async function AIResponse(){
-  const GROQ_API_KEY = process.env.API_KEY;;
+  const GROQ_API_KEY = API_KEY;;
   const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
     method: "POST",
     headers: {
